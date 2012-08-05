1bb23cee02766ae44779cef9d818da9a583abf38
Cin316 <joe13reich@gmail.com>
2012-08-05 10:37:59 -0400

    Added Grass class.

---
+++ src/com/earthforge/objSpec/Grass.java
@@ -0,0 +1,44 @@
+package com.earthforge.objSpec;
+
+import java.awt.Image;
+
+import com.earthforge.code.BlockDestroyer;
+import com.earthforge.code.Execute;
+import com.earthforge.obj.Block;
+import com.utilis.game.obj.CollisionBox;
+
+public class Grass extends Block{
+	
+	private long timeDiff = 0L;
+	private long startTime = 0L;
+	
+	//Image is of dirt.
+	public static Image image = Execute.readImageFromCodebase("images/Grass.png");
+	//CollisionBox is completely full.
+	public static CollisionBox collide = new CollisionBox(Execute.tileWidth, Execute.tileHeight, true);
+	//How long it takes to destroy Grass.  Measured in milliseconds.
+	public static long destroyTime = 380;
+	
+	public Grass(int x, int y) {
+		super(image, collide, x, y);
+		hasAction = true;
+	}
+	
+	@Override
+	public void performAction() {
+		if (startTime == 0L){
+			startTime = System.currentTimeMillis();
+		}
+		long time = System.currentTimeMillis();
+		timeDiff = time - startTime;
+		if (timeDiff >= destroyTime){
+			BlockDestroyer.destroy(this, null); //Add Grass entity. in place of null.
+		}
+	}
+	
+	public void resetTime(){
+		timeDiff = 0L;
+		startTime = 0L;
+	}
+	
+}
