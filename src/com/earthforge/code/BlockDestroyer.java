bbe8c02d8950243f9a816ff2659f7bd385947051
Cin316 <joe13reich@gmail.com>
2012-08-23 08:51:53 -0400

    Added Air block and modified BlockDestroyer to use it instead of null.

--- src/com/earthforge/code/BlockDestroyer.java
+++ src/com/earthforge/code/BlockDestroyer.java
@@ -2,11 +2,12 @@ package com.earthforge.code;
 
 import com.utilis.game.obj.*;
 import com.earthforge.obj.*;
+import com.earthforge.objSpec.Air;
 
 public class BlockDestroyer {
 	
 	public static void destroy(Block b, Entity drop){
-		b = null;
+		b = new Air(b.getHeight(), b.getWidth());
 		if(!(drop == null)){
 			//Spawn drop.
 		}
---
+++ src/com/earthforge/objSpec/Air.java
@@ -0,0 +1,28 @@
+package com.earthforge.objSpec;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import com.earthforge.code.Execute;
+import com.earthforge.obj.Block;
+import com.utilis.game.obj.CollisionBox;
+
+public class Air extends Block {
+	
+	//Image is blank.
+	public static Image image = new BufferedImage(Execute.tileWidth, Execute.tileHeight, BufferedImage.TYPE_INT_ARGB);
+	//CollisionBox is empty.
+	public static CollisionBox collide = new CollisionBox(Execute.tileWidth, Execute.tileHeight, false);
+	//How long it takes to destroy Air.  Measured in milliseconds.
+	public static long destroyTime = 0;
+	
+	public Air(int x, int y) {
+		super(image, collide, x, y);
+		hasAction = false;
+	}
+	
+	public void performAction() {
+
+	}
+
+}
