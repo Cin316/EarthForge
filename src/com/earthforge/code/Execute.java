eaf6d92cf07e7b2967715a54f3a4414607dcf2d4
Cin316 <joe13reich@gmail.com>
2012-08-26 09:31:59 -0400

    Modifed readImageFromCodebase to not use /bin. Added String version and set it to "Alpha 0.1".

--- src/com/earthforge/code/Execute.java
+++ src/com/earthforge/code/Execute.java
@@ -1,24 +1,31 @@
 package com.earthforge.code;
 
+import java.awt.Color;
 import java.awt.Image;
 import java.io.File;
 import java.net.URLDecoder;
 
 import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import com.earthforge.obj.*;
+import com.utilis.game.gui.*;
+import com.utilis.game.obj.Screen;
+import com.utilis.game.obj.ScrollingEntity;
 import com.utilis.Utilis;
 
 public class Execute {
 	
 	public static final int tileWidth = 32;
 	public static final int tileHeight = 32;
-	public static final int worldHeight = 128;
+	public static final int worldHeight = 15;
+	public static final String version = "v. Alpha 0.1";
 	
 	public static void main(String[] args) {
 		
-		System.out.println("EarthForge");
-		
+		System.out.println("EarthForge " + version + ".");
+	
 	}
 	
 	public static Image readImageFromCodebase(String imageName){
@@ -26,7 +33,9 @@ public class Execute {
 		//Gets and creates image object.
 		try {
 			//Finds location of were the program is running and sets codeBase to it.
-			String path = Utilis.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+			String path = Execute.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+			//Removes "/bin" from path.
+			path = path.substring(0, (path.length()-1) - 4 );
 			String decodedPath = URLDecoder.decode(path, "UTF-8");
 			//Creates image using decodePath.
 			File img1 = new File(decodedPath+imageName);
@@ -36,6 +45,7 @@ public class Execute {
 		}catch (Exception e) {
 			//Error message
 			JOptionPane.showMessageDialog(null, "There was an error: \n"+e, "Error!", 1);
+			e.printStackTrace();
 			return null;
 		}
 		
